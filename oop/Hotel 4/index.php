67dc299f8b4eee2e4243b1723afb30493b5ae02f
Pedro <93782349+pedropkm@users.noreply.github.com>
2022-08-16 21:55:38 +0200

    Add files via upload

---
+++ oop/Hotel 4/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Booking</title>
+</head>
+<body>
+<div class="form">
+      <div class="title">Book your Hotel</div>
+      <div class="subtitle"></div>
+      <div class="input-container ic1">
+        <input id="firstname" class="input" type="text" placeholder=" First Name" />
+      <div class="input-container ic2">
+        <input id="lastname" class="input" type="text" placeholder=" lastname" />
+      <div class="input-container ic2">
+        <input id="email" class="input" type="text" placeholder="Email Address" />
+      </div>
+      <h2 class="check">Check In Date</h2>
+        <div class="input-container ic3">
+          <input type="date" class="input">
+        </div>
+        <h2 class="check2">Check Out Date</h2>
+        <div class="input-container ic4">
+        <input type="date" class="input">
+        </div>
+      <button type="text" class="submit">Compare Price</button>
+    </div>
+</body>
+</html>
